5f3c68f7879c68a29bd1cd23067b83cd99ded643
misa198 <csnsjh@gmail.com>
2021-01-20 10:49:51 +0700

    line chart

---
+++ src/components/Chart.vue
@@ -0,0 +1,48 @@
+<script>
+import { Line } from "vue-chartjs";
+
+const options = {
+  scales: {
+    yAxes: [
+      {
+        ticks: {
+          beginAtZero: false,
+          fontFamily: "Roboto",
+          callback: function(value) {
+            return new Intl.NumberFormat("de-DE", {
+              style: "currency",
+              currency: "VND"
+            }).format(value);
+          }
+        }
+      }
+    ]
+  },
+  tooltips: {
+    callbacks: {
+      label: function(tooltipItem) {
+        return new Intl.NumberFormat("de-DE", {
+          style: "currency",
+          currency: "VND"
+        }).format(tooltipItem.value);
+      },
+      labelColor: function() {
+        return {
+          borderColor: "#ee4d2d",
+          backgroundColor: "#ee4d2d"
+        };
+      }
+    }
+  }
+};
+
+export default {
+  extends: Line,
+  props: ["chartdata"],
+  mounted() {
+    this.renderChart(this.chartdata, options);
+  }
+};
+</script>
+
+<style></style>
